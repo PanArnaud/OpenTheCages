8390d774ba7a3b794f2e3ca76e87ce1fcbcc1b26
Arnaud Panapadéatchy <arnaud.panapadeatchy@dsinstruments.fr>
2019-07-03 09:49:20 +0400

    Fix div on index.blade

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -10,7 +10,7 @@
 
   <main class="lg:flex lg:flex-wrap -mx-3">
     @forelse ($events as $event)
-      <div class="lg:w-1/3 px-3 pb-6"></div>
+      <div class="lg:w-1/3 px-3 pb-6">
         @include('events.card')
       </div>
     @empty
